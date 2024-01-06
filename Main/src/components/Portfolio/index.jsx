e31426e2065c7bf157741d926748603e748c2a99
Victor <frozen711.vr.vr@gmail.com>
2024-01-06 12:45:55 -0800

    updated portfolio page and added my projects on it

--- Main/src/components/Portfolio/index.jsx
+++ Main/src/components/Portfolio/index.jsx
@@ -6,34 +6,34 @@ function Portfolio() {
   // Replace links with deployed projects and GitHub repos
   const [projects] = useState([
     {
-      name: 'surfReport',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'Movie Nest',
+      description: 'Movie search and random activity APIs',
+      link: "https://lundj227.github.io/movie-nest/",
+      repo: "https://github.com/lundj227/movie-nest"
     },
     {
-      name: 'pastelPuzzels',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'password generator',
+      description: 'generates a random password',
+      link: "https://vromero-beltran.github.io/RandomPassGen/",
+      repo: "https://github.com/vromero-beltran/RandomPassGen"
     },
     {
-      name: 'runBuddy',
-      description: 'HTML/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'Tackle Time',
+      description: 'Football team tracker',
+      link: "https://kpfenning.github.io/Tackle-Times/",
+      repo: "https://github.com/kpfenning/Tackle-Times"
     },
     {
-      name: 'ledWall',
-      description: 'Node/IoT',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'README.md Generator',
+      description: 'Generates a README.md file',
+      link: "https://vromero-beltran.github.io/Repo-Application/",
+      repo: "https://github.com/vromero-beltran/Repo-Application"
     },
     {
-      name: 'calculator',
-      description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'Tech Blogging',
+      description: 'application that allows users to add friends, post, comment and react on tech blogs',
+      link: "https://vromero-beltran.github.io/tech-blogging/",
+      repo: "hhttps://github.com/vromero-beltran/tech-blogging"
     },
   ]);
 
